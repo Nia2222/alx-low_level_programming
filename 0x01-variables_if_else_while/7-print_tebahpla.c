02840c5b0f231aa62f8ec2a324d366c42f9c7d43
Nia2222 <aburawenada@gmail.com>
2022-12-09 08:43:46 -0800

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return single integer from 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
