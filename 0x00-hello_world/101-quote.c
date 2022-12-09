1e028958456574c95744b9e41b11972264042c01
Nia2222 <aburawenada@gmail.com>
2022-12-09 07:10:49 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
 
@@ -8,7 +7,7 @@
  * main - prints exactly and that piece of art is useful
  *
  * Return: Always 0 (Success)
- **/
+ */
 
 int main(void)
 
