040048844530177c1b4b9485ebabcb0d8e6382cb
Nia2222 <aburawenada@gmail.com>
2022-12-09 08:31:33 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns alphabets both except e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
