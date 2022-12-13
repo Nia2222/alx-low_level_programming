4783a54d18da5986583138066975b247586cfeda
Nia2222 <aburawenada@gmail.com>
2022-12-13 04:10:50 -0800

    fibonacci 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -30,7 +30,7 @@ int numLength(int num)
 
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, tmpo = 0;
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
 	short int i = 1, initial0s;
 
 	while (i < 98)
