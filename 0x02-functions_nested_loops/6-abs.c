8b11ca0a458753a3c265d865f92cae73395004fd
Nia2222 <aburawenada@gmail.com>
2022-12-13 02:04:54 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,7 @@
 /**
  * _abs - computes the absolute value of an integer
  * @n: The integer to be operated upon
+ *
  * Return: returns an unsigned int value to calling function
  */
 
@@ -12,9 +13,10 @@ int _abs(int n)
 	{
 		return (n);
 	}
+
 	else
 	{
-		n*= -1;
+		n *= -1;
 		return (n)
 	}
 }
