d30e98c8df89df3a1752e79248e9407fa58cd741
Nia2222 <aburawenada@gmail.com>
2022-12-13 01:48:13 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks if a charcater is lowercase if YES it returns 1
  * otherwise returns 0 just like islower() function in ctype.h
- *
+ * @c: Is the int value to be compared with ASCII value
  * Return: 0 if successful. _islower - Entry point
  */
 
