a19766cbeb47a52f5939d63f61ad738fc954349c
Nia2222 <aburawenada@gmail.com>
2022-12-13 02:30:16 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,13 +19,11 @@ void times_table(void)
 				{
 					_putchar('0');
 				}
-				
 				if (b != 0)
 				{
 					_putchar(' ');
 					_putchar((c % 10) + '0');
 				}
-
 				if (b < 9)
 				{
 					_putchar(',');
