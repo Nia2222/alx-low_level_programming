ef2ceb4f02d02d18aaeaa6712755edd786952f88
Nia2222 <aburawenada@gmail.com>
2023-01-12 06:04:09 -0800

    my task

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The integer to be operated upon
+ *
+ * Return: returns an unsigned int value to calling function
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
