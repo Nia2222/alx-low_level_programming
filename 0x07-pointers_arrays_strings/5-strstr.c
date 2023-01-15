fad9f47298058844d91ed08d6d04062b3b86d73f
Nia2222 <aburawenada@gmail.com>
2023-01-15 07:58:05 -0800

    my task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,18 +14,15 @@ char *_strstr(char *haystack, char *needle)
 		char *l = haystack;
 		char *p = needle;
 
-	
 		while (*l == *p && *p != '\0')
 		{
 			l++;
 			p++;
 		}
 
-	
 		if (*p == '\0')
 			return (haystack);
 	}
-
 
 	return (0);
 }
