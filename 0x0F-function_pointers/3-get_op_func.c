76452cff1409dff63093148cdd0d964b7cc75fb2
Nia2222 <aburawenada@gmail.com>
2023-01-18 02:31:00 -0800

    my task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -12,16 +12,16 @@ int (*get_op_func(char *s))(int, int)
 	op_t ops[] = {
 
 		{"+", op_add},
-	
+
 		{"-", op_sub},
-	
+
 		{"*", op_mul},
-	
+
 		{"/", op_div},
-	
+
 		{"%", op_mod},
-	
-		{NULL, NULL},	
+
+		{NULL, NULL},
 	};
 
 	int i = 0;
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,10 +13,11 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);	
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
@@ -26,13 +27,13 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(99);	
+		exit(99);
 	}
 
 	if ((*op == '/' && num2 == 0) ||  (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(100);	
+		exit(100);
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
