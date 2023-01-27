948f0a0e4b0550c531c912981efdfc864942177a
Nia2222 <aburawenada@gmail.com>
2023-01-27 04:51:11 -0800

    my task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,17 +15,17 @@ size_t print_list(const list_t *h)
 	{
 
 		if (!h->str)
-	
+
 			printf("[0] (nil)\n");
-		
+
 		else
-	
+
 			printf("[%u] %s\n", h->len, h->str);
-		
+
 		h = h->next;
-	
+
 		s++;
-	
+
 	}
 
 	return (s);
