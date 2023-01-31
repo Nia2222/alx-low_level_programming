ca6f76bde3ba5c9a969e3727b306350d6d524d8f
Nia2222 <aburawenada@gmail.com>
2023-01-31 10:11:10 -0800

    my task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,10 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
+
 	listint_t *new;
 	listint_t *temp = *head;
+
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
 		return (NULL);
@@ -22,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = *head;	
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
@@ -36,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new);
 		}
 		else
-			temp = temp->next;	
+			temp = temp->next;
 	}
 	return (NULL);
 }
