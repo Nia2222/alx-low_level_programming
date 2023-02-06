0793265b5307b4b8f73f30098d3012afc2baa820
Nia2222 <aburawenada@gmail.com>
2023-02-06 08:21:38 -0800

    my task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-			len++;		
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
